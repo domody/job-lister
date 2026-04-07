d0d4c16190470cac5d840d3986ec232f7f373e48
domody <domody11@gmail.com>
2026-04-07 20:04:03 +0100

    feat: add admin links to topbar

--- resources/js/layouts/public/public-topbar.tsx
+++ resources/js/layouts/public/public-topbar.tsx
@@ -1,7 +1,11 @@
 import AppLogoIcon from '@/components/app-logo-icon';
+import { Button } from '@/components/ui/button';
+import { usePage } from '@inertiajs/react';
 import { Link } from '@inertiajs/react';
 
 export default function PublicTopbar() {
+    const { auth } = usePage().props;
+
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container mx-auto flex h-14 items-center px-4">
@@ -9,8 +13,20 @@ export default function PublicTopbar() {
                     <div className="flex aspect-square size-7 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
                         <AppLogoIcon className="size-4 text-white dark:text-black" />
                     </div>
-                    <span className="font-semibold text-sm">Job Lister</span>
+                    <span className="text-sm font-semibold">Job Lister</span>
                 </Link>
+                {/* Right Aligned content */}
+                <div className="ml-auto flex items-center justify-end gap-2">
+                    {auth.user ? (
+                        <Button size={'sm'} variant={'secondary'} asChild>
+                            <Link href={'/dashboard'}>Dashboard</Link>
+                        </Button>
+                    ) : (
+                        <Button size={'sm'} variant={'secondary'} asChild>
+                            <Link href={'/login'}>Admin Login</Link>
+                        </Button>
+                    )}
+                </div>
             </div>
         </header>
     );
