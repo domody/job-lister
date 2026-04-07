ac3543b3949cc94047109ddc88de7dbb92f890c9
domody <domody11@gmail.com>
2026-04-07 18:15:11 +0100

    feat: implement job listings page

---
+++ resources/js/pages/public/jobs/list.tsx
@@ -0,0 +1,115 @@
+import { Badge } from '@/components/ui/badge';
+import {
+    InputGroup,
+    InputGroupAddon,
+    InputGroupInput,
+} from '@/components/ui/input-group';
+import {
+    Item,
+    ItemContent,
+    ItemDescription,
+    ItemGroup,
+    ItemTitle,
+} from '@/components/ui/item';
+import { Separator } from '@/components/ui/separator';
+import { Head, Link, usePage } from '@inertiajs/react';
+import { SearchIcon } from 'lucide-react';
+
+export default function JobList({ jobs }: { jobs: JobListing[] }) {
+    return (
+        <>
+            <Head title="Jobs" />
+            <div className="flex min-h-screen flex-col items-center">
+                {/* Hero */}
+                <div className="bg- w-full py-16">
+                    <div className="container mx-auto flex flex-col gap-2 px-4">
+                        <h1 className="text-4xl font-semibold">
+                            Find your dream job
+                        </h1>
+                        <p className="text-muted-foreground">
+                            Browse our latest job openings to view and apply to
+                            the best jobs today!
+                        </p>
+                    </div>
+                </div>
+
+                {/* Cont. */}
+                <div className="flex w-full flex-1 bg-background py-8">
+                    <div className="container mx-auto flex flex-col gap-2 px-4">
+                        <div className="flex flex-1 flex-col gap-2">
+                            {/* Search */}
+                            <InputGroup className="h-12">
+                                <InputGroupInput
+                                    id=""
+                                    placeholder="Search job title or keyword"
+                                />
+                                <InputGroupAddon align={'inline-start'}>
+                                    <SearchIcon className="text-muted-foreground" />
+                                </InputGroupAddon>
+                            </InputGroup>
+                            <Separator className="my-8" />
+                            {/* Jobs */}
+                            <div className="flex flex-row gap-2">
+                                {/* Filter */}
+                                <div className="h-96 w-[420px] rounded-sm border"></div>
+                                {/* List */}
+                                {jobs.length > 0 ? (
+                                    <ItemGroup className="flex-1 gap-2">
+                                        {jobs.map((job) => {
+                                            return (
+                                                <Item
+                                                    key={job.id}
+                                                    variant={'muted'}
+                                                    className="group/job hover:shadow"
+                                                    asChild
+                                                >
+                                                    <Link
+                                                        href={`/jobs/${job.id}`}
+                                                    >
+                                                        <ItemContent>
+                                                            <ItemTitle className="group-hover/job:underline">
+                                                                {job.title}
+                                                            </ItemTitle>
+                                                            <ItemDescription>
+                                                                {job.company},{' '}
+                                                                {job.location}
+                                                            </ItemDescription>
+                                                            <div className="mt-1 flex flex-row flex-wrap gap-2">
+                                                                <Badge
+                                                                    variant={
+                                                                        'secondary'
+                                                                    }
+                                                                >
+                                                                    £
+                                                                    {job.salary_min.toLocaleString()}{' '}
+                                                                    - £
+                                                                    {job.salary_max.toLocaleString()}
+                                                                </Badge>
+                                                                <Badge
+                                                                    variant={
+                                                                        'secondary'
+                                                                    }
+                                                                >
+                                                                    {job.type}
+                                                                </Badge>
+                                                            </div>
+                                                        </ItemContent>
+                                                    </Link>
+                                                </Item>
+                                            );
+                                        })}{' '}
+                                    </ItemGroup>
+                                ) : (
+                                    <div>Nothing here</div>
+                                )}
+
+                                {/* Detail Preview */}
+                                <div className=""></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
